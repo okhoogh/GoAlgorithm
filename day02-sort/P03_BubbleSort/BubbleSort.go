681477d00192946b3f7ddf04af72b060732e960e
Your Name <you@example.com>
2022-02-17 22:36:33 +0800

    Finished BubbleSort

--- day02-sort/P03_BubbleSort/BubbleSort.go
+++ day02-sort/P03_BubbleSort/BubbleSort.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func BubbleSort(arr []int) []int {
+	size := len(arr)
+	for i := 0; i < size-1; i++ {
+		flag := true
+		for j := 0; j < size-1-i; j++ {
+			arr[j], arr[j+1] = arr[j+1], arr[j]
+			flag = false
+		}
+		if flag {
+			break
+		}
+	}
+	return arr
+}
+
+func main() {
+	arr := []int{9, 8, 7, 6, 5, 4, 3, 2, 1}
+	fmt.Println(BubbleSort(arr))
 }
